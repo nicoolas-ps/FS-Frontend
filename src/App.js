ec0c855ad4a0ae26dd0c2d6cd414b57fa485fad2
nicoolasps <64236969+nicoolasps@users.noreply.github.com>
2021-10-10 14:44:32 -0300

    First Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './css/App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <section className="App-content">
+        Hola mundo
+      </section>
+    </div>
+  );
+}
+
+export default App;
